b6c848914196e5c10d9c8520efd483ae7f5cf867
vgamarra2021 <vgamarrav@uni.pe>
2023-12-26 03:09:14 -0500

    Add logs

--- locust-test/locustfile.py
+++ locust-test/locustfile.py
@@ -1,5 +1,11 @@
-from locust import FastHttpUser, task, between
+from locust import FastHttpUser, task, between, events
 from task_sets import MobileTaskSet, PcTaskSet
+
+def print_response(request_type, name, response_time, **kwargs):
+    print(f"Request type: {request_type}, Name: {name}, Response time: {response_time}")
+
+events.request_success.add_listener(print_response)
+events.request_failure.add_listener(print_response)
 
 class MobileUser(FastHttpUser):
     wait_time = between(1, 2)
