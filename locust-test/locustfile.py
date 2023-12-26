80996616f6c5885dfa162387449b371b971611f4
vgamarra2021 <vgamarrav@uni.pe>
2023-12-26 03:15:56 -0500

    update logs

--- locust-test/locustfile.py
+++ locust-test/locustfile.py
@@ -2,7 +2,7 @@ from locust import FastHttpUser, task, between, events
 from task_sets import MobileTaskSet, PcTaskSet
 
 def print_response(request_type, name, response_time, **kwargs):
-    print(f"Request type: {request_type}, Name: {name}, Response time: {response_time}")
+    print(f"Request type: {request_type}, Name: {name}, Response time: {response_time}, Response content: {response.text}")
 
 events.request_success.add_listener(print_response)
 events.request_failure.add_listener(print_response)
