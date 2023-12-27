bda3d067a049099e822e5345417b7640bb54eaae
victorgamarra <vgamarrav@uni.pe>
2023-12-27 03:21:08 -0500

    Add events

--- locust-test/locustfile.py
+++ locust-test/locustfile.py
@@ -1,14 +1,9 @@
 from locust import FastHttpUser, task, between, events
 from task_sets import MobileTaskSet, PcTaskSet
 
-@events.request_success.add_listener
-def on_request_success(request_type, name, response_time, response_length, response, **kwargs):
+@events.request.add_listener
+def on_request(request_type, name, response_time, response_length, response, **kwargs):
     print(f"Request type: {request_type}, Name: {name}, Response time: {response_time}, Response content: {response.text}")
-
-@events.request_failure.add_listener
-def on_request_failure(request_type, name, response_time, exception, **kwargs):
-    print(f"Request type: {request_type}, Name: {name}, Response time: {response_time}, Exception: {exception}")
-
 
 class MobileUser(FastHttpUser):
     wait_time = between(1, 2)
