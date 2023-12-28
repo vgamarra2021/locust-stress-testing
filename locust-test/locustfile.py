0ccaff123e0720c5cfdac1cc8a92a2fdc4c83f76
victorgamarra <vgamarrav@uni.pe>
2023-12-28 05:10:03 -0500

    low mem

--- locust-test/locustfile.py
+++ locust-test/locustfile.py
@@ -1,10 +1,12 @@
 from locust import FastHttpUser, task, between, events
 from task_sets import MobileTaskSet, PcTaskSet
 
+#EVENT
 @events.request.add_listener
 def on_request(request_type, name, response_time, response_length, response, **kwargs):
     print(f"Request type: {request_type}, Name: {name}, Response time: {response_time}, Response content: {response.text}")
 
+#USERS
 class MobileUser(FastHttpUser):
     wait_time = between(1, 2)
     
